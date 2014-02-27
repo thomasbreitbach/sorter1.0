852df85cda20a090283d605c1b9def681e1a63ef
thomasbreitbach <thomas.breitbach@iem.thm.de>
2014-02-27 18:19:33 +0100

    structure and comments

--- src/Sorter/readDir.clj
+++ src/Sorter/readDir.clj
@@ -1,14 +1,14 @@
 (ns Sorter.readDir
   (:use [clojure.string :only [join]])
   )
-;;For NOT nil eq !=
-(def not-nil? (complement nil?))
 
+;raw and image formats
 (def rawFormats ["CR2" "NEF" "RAW" "DNG"])
 (def imgFormats (conj rawFormats "jpg"))
 
-;case insesitiv regex pattern to retrieve only images
+;case insesitive regex pattern to retrieve only images
 (def img-regex
+  "case insesitive regex pattern to retrieve only images"
   (re-pattern (str ".+\\.(?i:(" (join "|"
                                  imgFormats) "))")))
 
@@ -20,33 +20,38 @@
 ;  "List all filenames in the given path as string array exl. this folder (./)"
 ;  (.list (clojure.java.io/file dir)))
 
-(defn count-files-of [dir]
+(defn count-files [dir]
   "counts files of the given directory."
   (count (list-files-as-file-seq dir)))
 
   
 ;list images (incl.  in given directory
 (defn list-images-with-path 
-  "gitb einen vector mit bildern zurück, die den gegebenen Dateiendungen entsprechen"
+  "gibt eine Liste aller Bilddateien (+ absoluter Pfad zur Datei) im gegebenen Ordner zurück"
   [dir]
   (filter #(re-find img-regex  %) (list-file-names-with-path dir)))
 
 (defn list-images
-  "gitb einen vector mit bildern zurück, die den gegebenen Dateiendungen entsprechen"
+  "gibt eine Liste mit Dateinamen von Bilddateien im gegegeben Ordner zurück"
   [dir]
   (filter #(re-find img-regex  %) (list-file-names dir)))
   
-;list only file names
-(defn list-file-names
+;list only filenames
+(defn list-filenames
+  "gibt eine Liste aller Dateinamen im gegebenen Ordner zurück"
   [dir]
-  (map #(.getName %) (list-files-as-file-seq dir))
-  )
+  (map #(.getName %) (list-files-as-file-seq dir)))
 
-(defn list-file-names-with-path
+
+(defn list-filenames-with-path
+  "gibt eine Liste aller Dateinamen (+ absoluter Pfad zur Datei) im gegebenen Ordner zurück"
   [dir]
   (map #(.getAbsolutePath %) (list-files-as-file-seq dir))
   )
 
+;###########################################
+;##############UNKNOWN STUFF################
+;##########################################
 
 ;(get rawFormats 0)
 ;(get rawFormats 1)
