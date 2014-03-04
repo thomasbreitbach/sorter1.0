a969218725635beae5e7533c8c1edaf55e9d1905
Andre Wißner <andrewissner@me.com>
2014-03-04 21:59:04 +0100

    added a new way for commandline

--- src/Sorter/main.clj
+++ src/Sorter/main.clj
@@ -29,6 +29,22 @@
 		       "Exit with Error : No match found!" ))
   )))
 
+(ns cmd-line-demo
+  (:gen-class)
+  (:use clojure.contrib.command-line))
+
+(defn -main [& args]
+  (with-command-line args
+      "Command line demo"
+      [[foo "This is the description for foo" 1]
+       [bar "This is the description for bar" 2]
+       [boolean? b? "This is a boolean flag."]
+       remaining]
+    (println "foo: " foo)
+    (println "bar: " bar)
+    (println "boolean?: " boolean?)
+    (println "remaining: " remaining)))
+
 (def pathToPictureDir "Z:/")
 (def pictures (list-images-with-path pathToPictureDir))
  (def pictureName (list-images pathToPictureDir))
