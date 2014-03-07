030c3888a1c6a5f857af106d14273bca2f4b94e8
thomasbreitbach <thomas.breitbach@iem.thm.de>
2014-03-07 11:44:15 +0100

    remove ns includeExternalLibs

--- src/Sorter/includeExternalLibs.clj
+++
@@ -1,7 +0,0 @@
-(ns Sorter.includeExternalLibs
-  (import '(com.drew.imaging.ImageMetadataReader ImageMetadataReader)
-           '(com.drew.imaging.ImageProcessingException ImageProcessingException)
-           '(com.drew.metadata.Directory Directory)
-           '(com.drew.metadata.Metadata Metadata)
-           '(com.drew.metadata.Tag Tag)))
-
